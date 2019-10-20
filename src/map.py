1e93df40011018250e8be282d1f7eea56ec4e2f7
EHoll <ehollidge@hotmail.com>
2019-10-20 18:33:30 +0200

    Removing Edges

--- src/map.py
+++ src/map.py
@@ -11,9 +11,8 @@ class Map:
 
 
 class Map_3d:
-    def __init__(self, verts, edges, width, height):
+    def __init__(self, verts, width, height):
         self.verts = verts
-        self.edges = edges
         self.width = width
         self.height = height
 
@@ -30,13 +29,26 @@ class Map_3d:
                 self.draw_trig(self.verts[i],
                                self.verts[i - self.width + 1],
                                self.verts[i + 1])
-                #self.draw_lines(self.verts[i],
-                #                self.verts[i - self.width],
-                #                self.verts[i - self.width + 1],
-                #                self.verts[i + 1])
                 x += 1
             y += 1
         glEnd()
+
+    def draw_trig(self, vert_1, vert_2, vert_3):
+        glColor3fv(self.get_color(vert_1[2]))
+        glVertex3fv(vert_1)
+        glColor3fv(self.get_color(vert_2[2]))
+        glVertex3fv(vert_2)
+        glColor3fv(self.get_color(vert_3[2]))
+        glVertex3fv(vert_3)
+
+    def get_color(self, y):
+        b_c = (0.92, 0.84, 0.41)
+        a_c = (0.1, 0.82, 0)
+        per = y / c.MAX_HEIGHT
+        x = ((a_c[0] - b_c[0]) * per) + b_c[0]
+        y = ((b_c[1] - a_c[1]) * per) + a_c[1]
+        z = ((b_c[2] - a_c[2]) * per) + a_c[2]
+        return x, y, z
 
     def draw_lines(self, vert_1, vert_2, vert_3, vert_4):
         c_1 = self.get_color(vert_1[2])
@@ -63,20 +75,3 @@ class Map_3d:
         glVertex3fv(vert_4)
         glColor3fv(c_3)
         glVertex3fv(vert_3)
-
-    def draw_trig(self, vert_1, vert_2, vert_3):
-        glColor3fv(self.get_color(vert_1[2]))
-        glVertex3fv(vert_1)
-        glColor3fv(self.get_color(vert_2[2]))
-        glVertex3fv(vert_2)
-        glColor3fv(self.get_color(vert_3[2]))
-        glVertex3fv(vert_3)
-
-    def get_color(self, y):
-        b_c = (0.92, 0.84, 0.41)
-        a_c = (0.1, 0.82, 0)
-        per = y / c.MAX_HEIGHT
-        x = ((a_c[0] - b_c[0]) * per) + b_c[0]
-        y = ((b_c[1] - a_c[1]) * per) + a_c[1]
-        z = ((b_c[2] - a_c[2]) * per) + a_c[2]
-        return (x, y, z)
--- src/terrain.py
+++ src/terrain.py
@@ -2,17 +2,12 @@ import map
 
 def get_map_3d(map_data):
     verts = []
-    edges = []
     y = 0
     while y < map_data.height:
         x = 0
         while x < map_data.width:
             i = (y * map_data.width) + x
             verts.append((x, y, int(map_data.array[i])))
-            if (x + 1) % map_data.width != 0:
-                edges.append((i, i + 1))
-            if y > 0:
-                edges.append((i, i - map_data.width))
             x += 1
         y += 1
-    return map.Map_3d(verts, edges, map_data.width, map_data.height)
+    return map.Map_3d(verts, map_data.width, map_data.height)
